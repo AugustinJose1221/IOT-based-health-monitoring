add3c960806f0d3685b7556083817ee32a5ccbf2
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-05-01 12:34:21 +0530

    Update Receive.py

--- Receive.py
+++ Receive.py
@@ -6,16 +6,49 @@ void setup()
 s.begin(9600);
 Serial.begin(9600);
 }
- 
+int x=1;
 void loop() 
 {
-  s.write("s");
+  //s.write("s");
   if (s.available()>0)
   {
-    data=s.read();
-    Serial.println(data);
-    Serial.print("True");
+    if(x%7==1)
+    {
+      data=s.read();
+      Serial.println(data);
+    }
+    if(x%7==2)
+    {
+      data=s.read();
+      Serial.println(data);
+    }
+    if(x%7==3)
+    {
+      data=s.read();
+      Serial.println(data);
+    }
+    if(x%7==4)
+    {
+      data=s.read();
+      Serial.println(data);
+    }
+    if(x%7==5)
+    {
+      data=s.read();
+      Serial.println(data);
+    }
+    if(x%7==6)
+    {
+      data=s.read();
+      Serial.println(data);
+    }
+    if(x%7==0)
+    {
+      Serial.println("**********");
+      x=1;
+    }
+    x++;
   }
- 
+  
  
 }
