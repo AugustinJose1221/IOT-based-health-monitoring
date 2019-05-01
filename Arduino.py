fe6f6fd69158a4384e586ac52ebca130f81efc75
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-05-01 12:08:40 +0530

    Create Arduino.py

---
+++ Arduino.py
@@ -0,0 +1,77 @@
+
+#include <SoftwareSerial.h>
+int sensorvalue;
+SoftwareSerial serial(9, 10);
+SoftwareSerial s(5, 6);
+void setup() 
+{
+  Serial.begin(9600);
+  serial.begin(9600);
+  s.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A4,INPUT);
+}
+float p = 0.0;
+float t = 0.0;
+float t1 = 0.0;
+float b = 0.0;
+void loop() 
+{
+  Serial.println(pulse());
+  Serial.println(temp());
+  Serial.println(beat());
+  bp();
+  Serial.println("*********");
+  
+}
+float pulse()
+{
+  delay(20);
+  p = analogRead(A0);
+  s.write(p);
+  return p;
+}
+float temp()
+{
+  delay(10);
+  t = analogRead(A2);
+  t1=(5.0*t*1000.0)/(1024*10);
+  s.write(t1);
+  return t1;
+}
+float beat()
+{
+  
+  b = analogRead(A4);
+  s.write(b);
+  return b;
+}
+float bp()
+{
+   char serialData[30];
+   int lf = 10;
+   unsigned char read1,read2,read3; 
+  
+   while(serial.available())
+   {
+     serial.readBytesUntil(lf,serialData, 15);
+   }
+  
+   read1 = ((serialData[1]-'0')*100) + ((serialData[2]-'0')*10) +(serialData[3]-'0');
+   read2 = ((serialData[6]-'0')*100) + ((serialData[7]-'0')*10) +(serialData[8]-'0');
+   read3 = ((serialData[11]-'0')*100) + ((serialData[12]-'0')*10) +(serialData[13]-'0');
+  
+   Serial.println(int(read1));
+   Serial.println(int(read2));
+   Serial.println(int(read3)); 
+   Serial.println("*******");
+   s.write(int(read1));
+   s.write(int(read2));
+   s.write(int(read3));
+   
+   delay(100);
+  
+   Serial.flush();
+   serial.flush();  
+}
